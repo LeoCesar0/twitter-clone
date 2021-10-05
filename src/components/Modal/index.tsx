e46653a918bbcf053171fc47bb01faf79a3cb6d8
Leonardo César <leo_cesa@hotmail.com>
2021-10-05 13:34:28 -0300

    feat: Modal Close on Escape

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,18 +1,32 @@
 import { Background, Center } from "./styles";
+import { useEffect } from "react";
 
 interface IModal {
   isOpen: boolean;
-  setIsOpen: (isOpen: boolean) => void
-  onClose: () => void
+  setIsOpen: (isOpen: boolean) => void;
+  onClose: () => void;
 }
 
 const Modal: React.FC<IModal> = ({ children, isOpen, setIsOpen, onClose }) => {
-    const closeModal = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if(event.target === event.currentTarget){
-            setIsOpen(false)
-            onClose()
-        }
+  const closeModal = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.target === event.currentTarget) {
+      setIsOpen(false);
+      onClose();
     }
+  };
+
+  const closeESC = (event: { keyCode: number }) => {
+    if (event.keyCode === 27) {
+      setIsOpen(false);
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", closeESC);
+
+    return () => {document.removeEventListener("keydown", closeESC);}
+  }, []);
 
   return (
     <>
