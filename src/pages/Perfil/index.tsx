5b79816f0df5adec80554bb19d6f11c65fbd0c90
Leonardo César <leo_cesa@hotmail.com>
2021-10-14 13:33:00 -0300

    feat: Integrate Profile Page with Backend

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,27 +1,113 @@
 import PageWrapper from "../../components/PageWrapper";
-import { FixedContainer, FixedFirstChild, FixedSecondChild } from "./styles";
+import {
+  FixedContainer,
+  FixedFirstChild,
+  FixedSecondChild,
+  FollowerCountContainer,
+  ImageContainer,
+  InfoContainer,
+  MainContainer,
+} from "./styles";
 import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { FaCalendarAlt } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { apiWithAuth } from "../../services/api";
+import Capitalize from "../../utils/Capitalize";
+import FormatDate from "../../utils/FormatDate";
+import { toast } from "react-toastify";
+
+interface ITweet {
+  id: string;
+  content: string;
+}
+
+interface IPerfil {
+  id: string;
+  name: string;
+  bio: string | null;
+  username: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+  number_of_followers: number;
+  number_of_follows: number;
+  tweets: ITweet[];
+}
 
 function Perfil() {
+  const [profile, setProfile] = useState<IPerfil>();
+
+  const getProfile = async () => {
+    try {
+      const { data } = await apiWithAuth.get("/profile");
+
+      setProfile(data);
+    } catch (error) {
+      toast.error(error?.response?.data?.message[0] || "Algo deu Errado!")
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
   return (
     <PageWrapper
       fixedContent={
         <>
           <FixedContainer>
             <FixedFirstChild>
-              <Link to="/" >
-              <BsArrowLeft size="18px" />
+              <Link to="/">
+                <BsArrowLeft size="18px" />
               </Link>
             </FixedFirstChild>
             <FixedSecondChild>
-              <h1>Leozin</h1>
-              <h2>12 Tweets</h2>
+              {profile && (
+                <>
+                  <h1> {Capitalize(profile.name)} </h1>
+                  <h2> {profile.tweets.length} Tweets </h2>
+                </>
+              )}
             </FixedSecondChild>
           </FixedContainer>
         </>
       }
-    ></PageWrapper>
+    >
+      {profile && (
+        <MainContainer>
+          <ImageContainer>
+            <img
+              src={`https://robohash.org/${profile.username}`}
+              alt={profile.username}
+            />
+            <button>Editar perfil</button>
+          </ImageContainer>
+          <InfoContainer>
+            <h1>{Capitalize(profile.name)}</h1>
+            <h2>@{profile.username}</h2>
+            <p className="bio">{profile?.bio}</p>
+            <p className="joined_at">
+              <FaCalendarAlt></FaCalendarAlt>
+              Ingressou em{" "}
+              {FormatDate(profile.created_at).month +
+                " de " +
+                FormatDate(profile.created_at).year}
+            </p>
+            <FollowerCountContainer>
+              <p>
+                {" "}
+                <span>{profile.number_of_follows}</span> Seguindo
+              </p>
+              <p>
+                {" "}
+                <span>{profile.number_of_followers} </span> Seguidores
+              </p>
+            </FollowerCountContainer>
+          </InfoContainer>
+        </MainContainer>
+      )}
+    </PageWrapper>
   );
 }
 
--- src/pages/Perfil/styles.ts
+++ src/pages/Perfil/styles.ts
@@ -28,3 +28,83 @@ export const FixedSecondChild = styled.div`
     color: #6d777c;
   }
 `;
+
+export const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 22px;
+  border-bottom: 1px solid ${props => props.theme.color.borderColor};
+`;
+
+export const ImageContainer = styled.div`
+  padding: 20px;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  img {
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+  }
+
+  button {
+    height: max-content;
+    background: none;
+    color: white;
+    padding: 8px 16px;
+    border: 1px solid ${(props) => props.theme.color.borderColor};
+    border-radius: 53px;
+  }
+`;
+
+export const NameContainer = styled.div``;
+
+export const InfoContainer = styled.div`
+  padding-left: 24px;
+
+  h1 {
+    font-weight: bold;
+    font-size: 19px;
+    line-height: 23px;
+
+    color: #d9d9d9;
+  }
+
+    h2 {
+      font-size: 14px;
+      line-height: 17px;
+      color: #6d777c;
+      margin-bottom: 12px;
+    }
+
+    .bio {
+      font-size: 15px;
+      line-height: 18px;
+      color: #d9d9d9;
+      margin-bottom: 12px;
+    }
+
+    .joined_at {
+      font-size: 15px;
+      line-height: 18px;
+      color: #6d777c;
+      svg{
+          margin-right: 8px;
+      }
+    }
+`;
+
+export const FollowerCountContainer = styled.div`
+    margin-top: 12px;
+    display: flex;
+    gap: 8px;
+
+    p{
+        color: #6d777c;
+    }
+
+    span{
+        color: #d9d9d9;
+    }
+`;
