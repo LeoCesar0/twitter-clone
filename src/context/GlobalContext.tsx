f988f209464f881475b9d0f43b5333bbe89a376d
Leonardo César <leo_cesa@hotmail.com>
2021-10-17 19:59:21 -0300

    feat: Create setNameInAuth Function

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -14,6 +14,7 @@ interface IGlobalState {
   auth?: IAuth;
   setAuth: (auth: IAuth) => void;
   removeAuth: () => void;
+  setNameInAuth: (name: string) => void
 }
 
 const GlobalContext = createContext<IGlobalState>({} as IGlobalState);
@@ -40,8 +41,22 @@ export const GlobalStateProvider: React.FC = ({ children }) => {
     setAuthState(auth)
   }
 
+
+  const setNameInAuth = (name: string) => {
+    if(!auth) return
+    
+    //removeAuth()
+
+    const newAuth = auth
+      newAuth.user.name = name
+      localStorage.removeItem("@twitter:auth")
+      localStorage.setItem("@twitter:auth", JSON.stringify(newAuth))
+      //setAuth(newAuth)
+    
+  }
+
   return (
-    <GlobalContext.Provider value={{ auth, setAuth, removeAuth }}>
+    <GlobalContext.Provider value={{ auth, setAuth, removeAuth, setNameInAuth }}>
       {children}
     </GlobalContext.Provider>
   );
